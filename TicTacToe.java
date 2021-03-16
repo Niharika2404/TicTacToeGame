9db789ffccd1f4a1aa3c50028f5b87566b06bce9
Niharika2404 <niharikang24@gmail.com>
2021-03-16 19:52:35 +0530

    BoxIsCreated: Symbol assign to player and Symtem

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class TicTacToe {
 	private static char[] createBoard() {
@@ -7,8 +8,22 @@ public class TicTacToe {
 		}
 		return board;
 	}
+	private static char chooseUserInputs() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Letter1: ");
+		char letter = scan.next().charAt(0);
+		System.out.println("Enter the Letter2: ");
+		char letter2 = scan.next().charAt(0);
+		System.out.println("Choose the letter: ");
+		return scan.next().toUpperCase().charAt(0);
+	}
 	public static void main(String[] args) {
 		char[] board = createBoard();
+		chooseUserInputs();
+		char person = 'X';
+		char computer = '0';
+		System.out.println("choosen letter by player: " + person);
+		System.out.println("choosen letter by system: " + computer);
 	}
 
 }
